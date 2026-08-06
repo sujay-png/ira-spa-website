20a23ee8b9a913322a89c237f032e69431de691d
sujay.png <147098677+sujay-png@users.noreply.github.com>
2026-08-06 14:40:05 +0530

    Swap Node adapter for Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import icon from 'astro-icon';
-import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
 
 export default defineConfig({
-	site: 'https://iraspa.in',
-	integrations: [icon(), sitemap()],
-	output: 'server',
-	adapter: node({ mode: 'standalone' }),
+  output: 'server',
+  adapter: vercel(), 
+  integrations: [icon()]
 });
