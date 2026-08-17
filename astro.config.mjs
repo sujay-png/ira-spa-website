774cb3eab788763782c331463081efe9e1588faa
sujay.png <147098677+sujay-png@users.noreply.github.com>
2026-08-17 18:19:54 +0530

    fix: remove local vercel cache from

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel'; 
 import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
 
